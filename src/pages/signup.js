c75413cc9a9b3234cb1ecc659f89a05188791277
update <yu0862407432@gmail.com>
2022-01-13 09:52:36 +0700

    update

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,7 @@
 const SignUP = {
   render() {
     return /*html */ `
-    <div class="bg-grey-lighter min-h-screen flex flex-col bg-violet-50">
+    <div class="bg-grey-lighter min-h-screen flex flex-col bg-violet-500">
     <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
         <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 class="mb-8 text-3xl text-center">Sign up</h1>
