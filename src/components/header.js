d947800e35690589fc43de8e3d5bdb1ce0776be5
Tuan Tu <tu0862407432@gmail.com>
2022-02-11 11:21:57 +0700

    updatenew

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,23 @@
 const Header = {
-  render() {
-    return `
-        <div class="bg-orange-500">
+    render() {
+        return `
+        <div class="bg-orange-500 flex justify-between items-center pr-4">
             <ul class="flex">
-            <li><a href="#" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Home Page</a></li>
-            <li><a href="/#/about" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">About Page</a></li>
-            <li><a href="/#/product" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Product Page</a></li>
+              <li><a href="#" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Home Page</a></li>
+              <li><a href="/#/about" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">About Page</a></li>
+              <li><a href="/#/product" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Product Page</a></li>
             </ul>
+            <ul class="flex">
+            <li><a id="accountInfo" href="#" class="block px-4 py-5 hover:bg-indigo-500 hover:text-white">Home Page</a></li>
+            
+          </ul>
         </div>
         `;
-  },
+    },
+    afterRender() {
+    // lấy thông tin username từ localStorage và hiển thị ra ngài
+        const { username } = JSON.parse(localStorage.getItem("user"));
+        document.querySelector("#accountInfo").innerHTML = username;
+    },
 };
 export default Header;
--- src/main.js
+++ src/main.js
@@ -12,21 +12,21 @@ import Signup from "./pages/signup";
 const router = new Navigo("/", { linksSelector: "a", hash: true });
 
 const print = async (content, id) => {
-  document.querySelector("#app").innerHTML = await content.render(id);
-  if (content.afterRender) await content.afterRender();
+    document.querySelector("#app").innerHTML = await content.render(id);
+    if (content.afterRender) await content.afterRender();
 };
 
 router.on({
-  "/": () => print(HomePage),
-  "/about": () => print(AboutPage),
-  "/product": () => print(ProductPage),
-  "/news/:id": ({ data }) => print(DetailPage, data.id),
-  "/admin/dashboard": () => print(DashboardPage),
-  "/admin/products": () => console.log("admin product"),
-  "/admin/news": () => print(AdminNews),
-  "/admin/news/add": () => print(AdminNewsAdd),
-  "/signup": () => print(Signup),
-  "/signin": () => print(Signin),
+    "/": () => print(HomePage),
+    "/about": () => print(AboutPage),
+    "/product": () => print(ProductPage),
+    "/news/:id": ({ data }) => print(DetailPage, data.id),
+    "/admin/dashboard": () => print(DashboardPage),
+    "/admin/products": () => console.log("admin product"),
+    "/admin/news": () => print(AdminNews),
+    "/admin/news/add": () => print(AdminNewsAdd),
+    "/signup": () => print(Signup),
+    "/signin": () => print(Signin),
 });
 
 router.resolve();
